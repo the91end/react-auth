5f669c60deaf48db9fc84acc174fc07fc7331fd8
theo.hendy <the@ad-ins.com>
2022-05-17 14:02:59 +0700

    - refactor exportDefaultFrom

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import app from './app';
 
-export context from './context';
-export actions from './actions';
+export { default as context } from './context';
+export { default as actions } from './actions';
 
-export Router from './components/Router';
+export { default as Router } from './components/Router';
 
-export HomeRoute from './components/HomeRoute';
-export LoginRoute from './components/LoginRoute';
-export LogoutRoute from './components/LogoutRoute';
-export AuthenticatedRoute from './components/AuthenticatedRoute';
+export { default as HomeRoute } from './components/HomeRoute';
+export { default as LoginRoute } from './components/LoginRoute';
+export { default as LogoutRoute } from './components/LogoutRoute';
+export { default as AuthenticatedRoute } from './components/AuthenticatedRoute';
 
-export Authenticated from './components/Authenticated';
-export NotAuthenticated from './components/NotAuthenticated';
+export { default as Authenticated } from './components/Authenticated';
+export { default as NotAuthenticated } from './components/NotAuthenticated';
 
-export LoginLink from './components/LoginLink';
-export LogoutLink from './components/LogoutLink';
+export { default as LoginLink } from './components/LoginLink';
+export { default as LogoutLink } from './components/LogoutLink';
 
-export LoginForm from './components/LoginForm';
-export UserProfileForm from './components/UserProfileForm';
-export SignupForm from './components/SignupForm';
-export ResetPasswordForm from './components/ResetPasswordForm';
-export ChangePasswordForm from './components/ChangePasswordForm';
-export VerifyEmailView from './components/VerifyEmailView';
+export { default as LoginForm } from './components/LoginForm';
+export { default as UserProfileForm } from './components/UserProfileForm';
+export { default as SignupForm } from './components/SignupForm';
+export { default as ResetPasswordForm } from './components/ResetPasswordForm';
+export { default as ChangePasswordForm } from './components/ChangePasswordForm';
+export { default as VerifyEmailView } from './components/VerifyEmailView';
 
-export SocialLoginLink from './components/SocialLoginLink';
-export SocialLoginButton from './components/SocialLoginButton';
+export { default as SocialLoginLink } from './components/SocialLoginLink';
+export { default as SocialLoginButton } from './components/SocialLoginButton';
 
 // When not using ES6, enable people to use ReactAuth.init()
 // instead of ReactAuth.default.init().
